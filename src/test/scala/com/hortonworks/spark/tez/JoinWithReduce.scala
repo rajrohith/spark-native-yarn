63c514518366db282dcfae36947a55a8ffc9a0df
Oleg Zhurakousky <oleg@suitcase.io>
2014-07-23 12:15:35 -0400

    added separate sample for multi-stage DAG

---
+++ src/test/scala/com/hortonworks/spark/tez/JoinWithReduce.scala
@@ -0,0 +1,40 @@
+package com.hortonworks.spark.tez
+
+import org.apache.spark.Tez
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.yarn.conf.YarnConfiguration
+
+/**
+ * This sample is similar to SampleJoin.scala and demonstrates join between two sources
+ * However, unlike the SampleJoin.scala it reduces (merges) output by key and then sorts its which results
+ * in additional stage making it 4 stage (4 Vertexes) DAG
+ * 
+ * First two stages will read and map the file and their output will be read
+ * by a third stage and eventually fed into a fourth stage to do the sorting
+ */
+object JoinWithReduce extends App {
+  val fs = FileSystem.get(new YarnConfiguration);
+  var testFile = new Path("file1.txt");
+  fs.copyFromLocalFile(false, true, new Path("/Users/ozhurakousky/dev/spark-on-tez/file1.txt"), testFile);
+  testFile = new Path("file2.txt");
+  fs.copyFromLocalFile(false, true, new Path("/Users/ozhurakousky/dev/spark-on-tez/file2.txt"), testFile);
+
+  val sc = new SparkContext("local", "SampleJoin") with Tez
+
+  val s1 = sc.textFile("file1.txt")
+  val s2 = sc.textFile("file2.txt")
+
+  val result = s1.map { line => val x = line.split("\\s+"); (x(2), x(0) + "," + x(1)) }
+    .join(s2.map { line => val x = line.split("\\s+"); (x(0), x(1)) })
+    .reduceByKey { (x, y) =>
+      println("reducing");
+      (x.toString, y.toString)
+    }
+    .sortByKey(true)
+    .collect
+
+  println(result.toList)
+}
--- src/test/scala/com/hortonworks/spark/tez/SampleJoin.scala
+++ src/test/scala/com/hortonworks/spark/tez/SampleJoin.scala
@@ -10,6 +10,11 @@ import org.apache.spark.rdd.ShuffledRDD
 import org.apache.spark.rdd.RDD
 import org.apache.spark.RangePartitioner
 
+/**
+ * This sample demonstrates simple join between two sources
+ * It will consist of 3 stages (3 Vertexes DAG) where first two stages will read and map the file 
+ * and their output will be read by third stage
+ */
 object SampleJoin extends App {
   
   val fs = FileSystem.get(new YarnConfiguration);
@@ -25,10 +30,6 @@ object SampleJoin extends App {
 
   val result = s1.map{line => val x = line.split("\\s+"); (x(2), x(0) + "," +  x(1) )}
   	.join(s2.map{line => val x = line.split("\\s+"); (x(0), x(1))})
-  	.reduceByKey{(x, y) => 
-  	  println("reducing");
-  	  (x.toString, y.toString)}
-  	.sortByKey(true)
   	.collect
   	
   println(result.toList) 
