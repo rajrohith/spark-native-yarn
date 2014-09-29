94398f1f3905f8e4bbaf0ba8001259cc5a954f68
Oleg Zhurakousky <oleg@suitcase.io>
2014-09-29 10:10:08 -0400

    added Apache LICENSE header on all source files

--- src/main/scala/org/apache/spark/tez/adapter/SparkToTezAdapter.scala
+++ src/main/scala/org/apache/spark/tez/adapter/SparkToTezAdapter.scala
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.spark.tez.adapter
 
 import javassist.ClassPool
@@ -69,16 +85,18 @@ object SparkToTezAdapter extends Logging{
    * 
    */
   def adapt = {
-    logInfo("Adapting PairRDDFunctions.saveAsNewAPIHadoopDataset for Tez")
     val pairRddTargetMethods = pairRddFunctions.getDeclaredMethods
     for (targetMethod <- pairRddTargetMethods){
       if (targetMethod.getName() == "saveAsNewAPIHadoopDataset"){
+        logDebug("Adapting PairRDDFunctions.saveAsNewAPIHadoopDataset for Tez")
         this.swapPairRddFunctionsMethodBody(targetMethod)
       }
       else  if (targetMethod.getName() == "saveAsHadoopDataset"){
+        logDebug("Adapting PairRDDFunctions.saveAsHadoopDataset for Tez")
         this.swapPairRddFunctionsMethodBody(targetMethod)
       }
       else if (targetMethod.getName() == "groupByKey"){
+        logDebug("Adapting PairRDDFunctions.groupByKey for Tez")
         this.swapPairRddFunctionsMethodBody(targetMethod)
       }
     }
