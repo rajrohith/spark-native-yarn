712445641bff0aba8295812873ed33ebf9c12801
Oleg Zhurakousky <oleg@suitcase.io>
2014-10-01 10:00:19 -0400

    polished PartitionByMulti demo

--- src/main/scala/dev/demo/PartitionByMulti.scala
+++ src/main/scala/dev/demo/PartitionByMulti.scala
@@ -30,36 +30,36 @@ import org.apache.spark.SparkConf
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
- * This demo demonstrates one of the rudimentary ETL use cases such as partitioning source data.
- * Similar to the 'reduceBy', the data will be partitioned via 'partitionBy' and then
- * written as file-per-key into as many files as there are keys in the source file using
- * custom implementation of MultipleTextOutputFormat.
+ * This demo demonstrates one of the rudimentary ETL use cases such as partitioning source data
+ * in multi-tenant environment
  *
  */
 object PartitionByMulti {
 
   def main(args: Array[String]) {
-    var reducers = 1
-    var inputFile = "/user/hive/external/tpch-100/lineitem "
+    var reducers = 64
+    var inputFile = "/user/hive/external/tpch-100/lineitem"
+    var tenants = 1
     if (args != null && args.length > 0) {
       reducers = Integer.parseInt(args(0))
       if (args.length > 1) {
         inputFile = args(1)
       }
+      if (args.length > 2) {
+        tenants = Integer.parseInt(args(2))
+      }
     }
 
-    System.setProperty(TezConstants.GENERATE_JAR, "true");
-    System.setProperty(TezConstants.UPDATE_CLASSPATH, "false");
-    val tenants = 2
+//    System.setProperty(TezConstants.GENERATE_JAR, "true");
+//    System.setProperty(TezConstants.UPDATE_CLASSPATH, "false");
     val executor = Executors.newCachedThreadPool()
     val latch = new CountDownLatch(tenants)
     for (i <- 0 until tenants) {   
       executor.execute(new Runnable {
         def run() {
           try {
-            val jobName = "PartitionByMulti_" + System.currentTimeMillis()
+            val jobName = "PartitionByMulti_" + System.nanoTime()
             val outputPath = jobName + "_out"
-
             val masterUrl = "execution-context:" + classOf[TezJobExecutionContext].getName
             val conf = new SparkConf
             conf.set("spark.ui.enabled", "false");
@@ -70,7 +70,7 @@ object PartitionByMulti {
 
             val result = source
               .map(a => (a.split('|')(10), a))
-              .partitionBy(new HashPartitioner(32))
+              .partitionBy(new HashPartitioner(reducers))
               .saveAsHadoopFile(outputPath, classOf[Text], classOf[Text], classOf[KeyPerPartitionOutputFormat])
 
             sc.stop
@@ -81,12 +81,10 @@ object PartitionByMulti {
           }
         }
       });
-//      Thread.sleep(3000);
     }
 
     latch.await()
     executor.shutdown()
     println("DONE")
-    //DemoUtilities.printSampleResults(outputPath)
   }
 }
